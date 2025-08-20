eaf340b66b9e7d3e3ace77370232b2ca712d5523
Vishal Bharat <vishalbharat.vs@gmail.com>
2025-08-20 09:57:52 +0200

    Adding String JAVA docs

---
+++ Strings.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Strings {
+
+    public static void main(String[] args) {
+        String str = new String("Vishal");
+        String str2 = "Vishal";
+        System.out.println(str.matches("^v"));
+    }
+}
